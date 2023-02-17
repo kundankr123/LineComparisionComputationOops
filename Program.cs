032922fda2e048daee0716d16914fc9355ebd58f
kundan <goswamikundan123@gmail.com>
2023-02-17 14:03:32 +0530

    Adding file which displays Greeting message.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisionComputationOops
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation.");
+        }
+    }
+}
