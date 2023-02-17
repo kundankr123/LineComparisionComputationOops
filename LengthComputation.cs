30693abda73947d08b94c6ce99385700355b1345
kundan <goswamikundan123@gmail.com>
2023-02-17 14:15:45 +0530

    Adding file which computes length of a line based on coordinates.

---
+++ LengthComputation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComprasionComputationOops
+{
+    public class LengthComputation
+    {
+        public void CalculateLength()
+        {
+            double Lengthofline = 0;
+            Console.WriteLine("Enter the first point coordinate: ");
+            int x1=Convert.ToInt32(Console.ReadLine());
+            int y1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second point coordinate: ");
+            int x2=Convert.ToInt32(Console.ReadLine());
+            int y2=Convert.ToInt32(Console.ReadLine());
+
+            Lengthofline = Math.Sqrt(Math.Pow((y2 - y1), 2) + Math.Pow((x2 - x1), 2));
+
+            Console.WriteLine("The length of line segment is: " + Lengthofline);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace LineComparisionComputationOops
+﻿using LineComprasionComputationOops;
+
+namespace LineComparisionComputationOops
 {
     class Program
     {
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation.");
+
+            LengthComputation LineLength = new LengthComputation();
+            LineLength.CalculateLength();
         }
     }
 }
